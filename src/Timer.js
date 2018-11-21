5a8978581595d4ed817de96ac329920deddf3e28
github username <github email address>
2018-11-21 13:43:07 -0500

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -25,12 +25,14 @@ class Timer extends Component {
     clearInterval(this.interval)
   }
 
-  shouldComponentUpdate(){
-    
+  shouldComponentUpdate(nextProps, nextState){
+    if (this.state.time === nextState.time) {
+      return false;
+    }
+    return true
   }
 
   componentDidUpdate(prevProps){
-    this.timer.current.style.color = '#'+Math.floor(Math.random()*16777215).toString(16)    
   }
 
   
